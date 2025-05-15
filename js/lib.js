825c0018500ed004aebfe4f4db7401ba41020564
seulgi3927 <seulgi3927@gmail.com>
2025-05-15 14:14:10 +0900

    [docs] js 추가

--- js/lib.js
+++ js/lib.js
@@ -1,7 +1,6 @@
-// 대문자 즉 Pascal
-function Coffee(){
-  console.log(this);
+function Student(_name, age) {
+  this.name = _name;
+  this.age = _age;
 }
-
-Coffee();
-new Coffee();
+const student_1 = new Student("홍길동", 20);
+const student_2 = new Student("둘리", 2000);
